7b6e72fc58947b046eaaa3bf8d93230007c7ba59
Augusto Ludovice <srludovice@gmail.com>
2020-12-06 20:33:19 -0400

    Adicionando cursos já em andamento
    Curso HTML5 e CSS3 do Curso em Vídeo e Formação Nodejs

---
+++ formação-node/ex001/app.js
@@ -0,0 +1,10 @@
+var mostrarSite = true
+const site = 'www.augustoludovice.com'
+
+console.log('Hello Word!')
+console.log('Meu nome é Augusto!')
+console.log('E eu estou aprendendo Node.js com o Guia do programador')
+
+if(mostrarSite){
+    console.log(site)
+}
